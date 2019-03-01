665101e364ff3761bc90747a27eaa13b522b15bb
TranVietDuc <vietduc3k@gmail.com>
2019-03-01 11:45:25 +0700

    fifth commit

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
+// server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/wallet-keeper-angular'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/wallet-keeper-angular/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-apphero.listen(process.env.PORT || 8080);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
